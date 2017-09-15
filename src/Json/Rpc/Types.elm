a124f7185f3e4ce1064c2be71e0c516a82218326
Jungle <devex.soft@gmail.com>
2017-09-15 17:56:10 +0500

    Add simple documentation to Types

--- src/Json/Rpc/Types.elm
+++ src/Json/Rpc/Types.elm
@@ -1,31 +1,59 @@
-module Json.Rpc.Types exposing (..)
+module Json.Rpc.Types
+    exposing
+        ( Id(IdString, IdInt)
+        , Params(ParamsList, ParamsObject)
+        , Version
+        , Method
+        , Request
+        , Response
+        , ResponseResult
+        , Error
+        , version
+        , createRequest
+        , createNotification
+        )
+
+{-| Types and functions
+
+# Types
+@docs version, Id, Params, Version, Method, Request, Response, ResponseResult, Error
+
+# Functions
+@docs createRequest, createNotification
+-}
 
 import Json.Encode exposing (Value)
 
 
-version : String
-version =
-    "2.0"
-
-
+{-| Union type Id can be String or Int
+-}
 type Id
     = IdString String
     | IdInt Int
 
 
+{-| Union type Params can be (List Value) to represent Array
+or (List (String, Value)) to represent Object
+-}
 type Params
     = ParamsList (List Value)
     | ParamsObject (List ( String, Value ))
 
 
+{-| Type alias Version is a String of JSON-RPC version ("2.0")
+-}
 type alias Version =
     String
 
 
+{-| Type alias Method
+-}
 type alias Method =
     String
 
 
+{-| JSON-RPC Request type
+-}
 type alias Request =
     { jsonrpc : Version
     , method : Method
@@ -34,10 +62,14 @@ type alias Request =
     }
 
 
+{-| Type alias ResponseResult
+-}
 type alias ResponseResult =
     Result Error Value
 
 
+{-| Type alias Response
+-}
 type alias Response =
     { jsonrpc : Version
     , result : ResponseResult
@@ -45,6 +77,8 @@ type alias Response =
     }
 
 
+{-| Type alias Error
+-}
 type alias Error =
     { code : Int
     , message : String
@@ -52,6 +86,15 @@ type alias Error =
     }
 
 
+{-| Default version ("2.0") of JSON-RPC
+-}
+version : Version
+version =
+    "2.0"
+
+
+{-| Helper function to create Request
+-}
 createRequest : Method -> Maybe Params -> Maybe Id -> Request
 createRequest method params id =
     { jsonrpc = version
@@ -61,6 +104,8 @@ createRequest method params id =
     }
 
 
+{-| Helper function to create Notification
+-}
 createNotification : Method -> Maybe Params -> Request
 createNotification method params =
     createRequest method params Nothing
